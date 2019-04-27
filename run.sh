8ee6f2624356dd97612627a5b1788320f5e4d8b5
Ondrej Tom <odis@meiro.io>
2019-04-27 18:25:09 +0200

    Fixing comments

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Let's iterate all input SAM files
+# Let's iterate all input mpileup files
 for file_path in /data/in/files/*.mpileup.samtools; do
 
     # Let's extract the filename from the path
@@ -9,7 +9,7 @@ for file_path in /data/in/files/*.mpileup.samtools; do
     # Let's remove the SAM extension
     filename=${filename:0:-17}
 
-    # Execute the samtools on input file and save the output as .BAM file
+    # Execute the samtools on input file and save the output as .vcf file
     echo "Executing java -jar /app/VarScan.v2.4.3.jar mpileup2snp ${file_path} --strand-filter 0 --p-value 0.95 --min-coverage 50 --min-reads2 8 --min-avg-qual 25 --min-var-freq 0.01 --output-vcf  > /data/out/files/${filename}.vcf"
     java -jar /app/VarScan.v2.4.3.jar mpileup2snp ${file_path} --strand-filter 0 --p-value 0.95 --min-coverage 50 --min-reads2 8 --min-avg-qual 25 --min-var-freq 0.01 --output-vcf  > /data/out/files/${filename}.vcf
 done
